9fbe806cbfc8308a32e1a3222df94eb1c0dcde66
Scott Côté <scott@gadget.dev>
2023-05-03 17:06:01 -0400

    Have help use the base command

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,12 +1,13 @@
-import { Args, Command } from "@oclif/core";
+import { Args } from "@oclif/core";
 import Help from "../utils/help";
+import { BaseCommand } from "../utils/base-command";
 
 /**
  * Copied from @oclif/plugin-help. Uses our own {@link Help} template class instead of the one from @oclif/core.
  *
  * @see https://github.com/oclif/plugin-help/blob/67b580570257b45e92d3a04d50bf2a432c59afe3/src/commands/help.ts
  */
-export default class HelpCommand extends Command {
+export default class HelpCommand extends BaseCommand<typeof HelpCommand> {
   static override strict = false;
 
   static override summary = "Display help for ggt.";

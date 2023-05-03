bb8305e0fe8cc72e7c699f1765b197e41581eef9
Scott Côté <scott@gadget.dev>
2023-05-03 13:26:18 -0400

    Refactor flake.nix

--- flake.nix
+++ flake.nix
@@ -1,40 +1,29 @@
 {
-  description = "Gadget development environment";
+  description = "GGT development environment";
 
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, flake-utils, nixpkgs }:
-    (flake-utils.lib.eachSystem [
-      "x86_64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
-    ]
-      (system: nixpkgs.lib.fix (flake:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        rec {
-          packages =
-            rec {
-              git = pkgs.git;
+  outputs = { self, nixpkgs, flake-utils }:
+    (flake-utils.lib.eachDefaultSystem (system: nixpkgs.lib.fix (flake:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = {
+          git = pkgs.git;
+          mkcert = pkgs.mkcert;
+          nix-direnv = pkgs.nix-direnv.override { enableFlakes = true; };
+          nodejs = pkgs.nodejs-16_x;
+        };
 
-              nix-direnv = pkgs.nix-direnv.override {
-                enableFlakes = true;
-              };
-
-              nodejs = pkgs.nodejs-16_x;
-
-              mkcert = pkgs.mkcert;
-            };
-
-          devShell = pkgs.mkShell {
-            packages = builtins.attrValues packages;
-          };
-        }
-      )));
+        devShell = pkgs.mkShell {
+          packages = builtins.attrValues flake.packages;
+        };
+      }
+    )));
 
   nixConfig.bash-prompt = "\[ggt-develop:\\w\]$ ";
 }

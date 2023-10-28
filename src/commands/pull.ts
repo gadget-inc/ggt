5b15d7f2597d874d7fb8acdc32e12d13ac433326
Scott Côté <scott@gadget.dev>
2023-10-28 02:57:53 -0400

    WIP - Sat, Oct 28 02:57 AM

--- src/commands/pull.ts
+++ src/commands/pull.ts
@@ -1,6 +1,6 @@
 import arg from "arg";
 import { AppArg } from "../services/args.js";
-import { FileHashes, FileSync } from "../services/filesync.js";
+import { FileSync, type FileHashes } from "../services/filesync.js";
 import { createLogger } from "../services/log.js";
 import { println, println2, sprint } from "../services/output.js";
 import { confirm } from "../services/prompt.js";
@@ -33,6 +33,26 @@ export const run = async (rootArgs: RootArgs) => {
   const fileHashes = await filesync.fileHashes.unwrap();
 
   log.info("file hashes", fileHashes);
+
+  if (fileHashes.localToLocalOriginChanges.length > 0 && !args["--force"]) {
+    println2`{bold The following changes have been made to your local filesystem since the last sync}`;
+    fileHashes.localToLocalOriginChanges.printChangesMade();
+
+    println`
+      {bold You must either:}
+
+        1. Reset your local filesystem to the last time you ran file sync.
+
+             {gray ggt reset}
+
+        2. Re-run this command again with the {bold --force} flag.
+
+            {gray ggt pull --force}
+    `;
+
+    // TODO: just return 1 here instead of exiting
+    process.exit(1);
+  }
 
   if (fileHashes.latestOriginToLocalChanges.length === 0) {
     println("Your local files are already up to date!");

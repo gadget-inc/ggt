7f2a77be4ecd177f8594d08c35f954f9501e82e8
Scott Côté <scott@gadget.dev>
2023-10-29 23:42:14 -0400

    WIP - Sun, Oct 29 11:42 PM

--- spec/commands/root.spec.ts
+++ spec/commands/root.spec.ts
@@ -2,7 +2,7 @@ import debug from "debug";
 import { afterEach } from "node:test";
 import { dedent } from "ts-dedent";
 import { beforeEach, describe, expect, it, vi } from "vitest";
-import { availableCommands, type Command } from "../../src/commands/index.js";
+import { availableCommands, type CommandModule } from "../../src/commands/index.js";
 import { config } from "../../src/services/config.js";
 import { CLIError, IsBug } from "../../src/services/errors.js";
 import { noop } from "../../src/services/noop.js";
@@ -10,7 +10,7 @@ import * as version from "../../src/services/version.js";
 import { expectProcessExit, expectStdout } from "../util.js";
 
 describe("root", () => {
-  let run: () => Promise<void>;
+  let command: () => Promise<void>;
 
   beforeEach(async () => {
     // don't check for updates
@@ -20,7 +20,7 @@ describe("root", () => {
     // we have to mock this before importing root so that the top-level usage uses the mocked version
     vi.spyOn(config, "versionFull", "get").mockReturnValue("ggt/1.2.3 darwin-arm64 node-v16.0.0");
 
-    ({ run } = await import("../../src/commands/root.js"));
+    ({ command } = await import("../../src/commands/root.js"));
   });
 
   afterEach(() => {
@@ -31,7 +31,7 @@ describe("root", () => {
     process.argv = ["node", "ggt", "--debug"];
     vi.spyOn(debug, "enable").mockImplementation(noop);
 
-    await expectProcessExit(run);
+    await expectProcessExit(command);
 
     expect(debug.enable).toHaveBeenCalledWith(`ggt:*`);
   });
@@ -40,7 +40,7 @@ describe("root", () => {
     process.argv = ["node", "ggt", flag];
     vi.spyOn(config, "version", "get").mockReturnValue("1.2.3");
 
-    await expectProcessExit(run);
+    await expectProcessExit(command);
 
     expectStdout().toEqual("1.2.3\n");
   });
@@ -48,7 +48,7 @@ describe("root", () => {
   it("prints root usage when no command is given", async () => {
     process.argv = ["node", "ggt"];
 
-    await expectProcessExit(run);
+    await expectProcessExit(command);
 
     expectStdout().toMatchInlineSnapshot(`
       "The command-line interface for Gadget
@@ -78,7 +78,7 @@ describe("root", () => {
   it("prints out a helpful message when an unknown command is given", async () => {
     process.argv = ["node", "ggt", "foobar"];
 
-    await expectProcessExit(run, 1);
+    await expectProcessExit(command, 1);
 
     expectStdout().toMatchInlineSnapshot(`
       "Unknown command foobar
@@ -91,33 +91,33 @@ describe("root", () => {
   });
 
   describe.each(availableCommands)("when %s is given", (name) => {
-    let command: Command;
+    let mod: CommandModule;
 
     beforeEach(async () => {
-      command = await vi.importActual(`../../src/commands/${name}.js`);
-      vi.spyOn(command, "run").mockImplementation(noop);
-      if (command.init) {
-        vi.spyOn(command, "init").mockImplementation(noop);
+      mod = await vi.importActual(`../../src/commands/${name}.js`);
+      vi.spyOn(mod, "command").mockImplementation(noop);
+      if (mod.init) {
+        vi.spyOn(mod, "init").mockImplementation(noop);
       }
     });
 
     it.each(["--help", "-h"])("prints the usage when %s is passed", async (flag) => {
       process.argv = ["node", "ggt", name, flag];
 
-      await expectProcessExit(run);
+      await expectProcessExit(command);
 
-      expectStdout().toEqual(command.usage + "\n");
+      expectStdout().toEqual(mod.usage + "\n");
     });
 
     it("runs the command", async () => {
       process.argv = ["node", "ggt", name];
 
-      await run();
+      await command();
 
-      if (command.init) {
-        expect(command.init).toHaveBeenCalled();
+      if (mod.init) {
+        expect(mod.init).toHaveBeenCalled();
       }
-      expect(command.run).toHaveBeenCalled();
+      expect(mod.command).toHaveBeenCalled();
     });
 
     it("captures errors", async () => {
@@ -137,9 +137,9 @@ describe("root", () => {
       vi.spyOn(error, "capture");
 
       process.argv = ["node", "ggt", name];
-      vi.spyOn(command, "run").mockRejectedValue(error);
+      vi.spyOn(mod, "command").mockRejectedValue(error);
 
-      await expectProcessExit(run, 1);
+      await expectProcessExit(command, 1);
 
       expectStdout().toEqual(
         dedent`

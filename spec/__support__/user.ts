7aaedc2596c6ef7f0cd415ef610197459090fe01
Scott Côté <scott.cote@hey.com>
2023-12-05 14:19:49 -0500

    Document __support__/user

--- spec/__support__/user.ts
+++ spec/__support__/user.ts
@@ -1,18 +1,25 @@
 import nock from "nock";
+import { randomUUID } from "node:crypto";
 import { expect } from "vitest";
 import { config } from "../../src/services/config/config.js";
 import { writeSession } from "../../src/services/user/session.js";
 import type { User } from "../../src/services/user/user.js";
 import { loadCookie } from "../../src/services/util/http.js";
 
-export const testUser: User = {
+/**
+ * A test user to use in tests.
+ */
+export const testUser: User = Object.freeze({
   id: 1,
   email: "test@example.com",
   name: "Jane Doe",
-};
+});
 
+/**
+ * Sets up a response for the current-user endpoint that `getUser` uses.
+ */
 export const loginTestUser = ({ optional = true } = {}): void => {
-  writeSession("test");
+  writeSession(randomUUID());
 
   nock(`https://${config.domains.services}`)
     .get("/auth/api/current-user")

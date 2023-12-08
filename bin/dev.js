e18dee9b00b7e5eeccf7871d1b28ab0b72f05379
Scott Côté <scott.cote@hey.com>
2023-12-08 00:10:17 -0500

    Use workspacePath in dev.js

--- bin/dev.js
+++ bin/dev.js
@@ -1,16 +1,13 @@
 #!/usr/bin/env node --loader @swc-node/register/esm --no-warnings
 
-import { dirname, join } from "node:path";
 import process from "node:process";
-import { fileURLToPath } from "node:url";
-
-const workspaceRoot = join(dirname(fileURLToPath(import.meta.url)), "..");
+import { workspacePath } from "../src/services/config/paths.js";
 
 process.env["NODE_ENV"] ??= "development";
 process.env["GGT_ENV"] ??= "development";
 process.env["GGT_SENTRY_ENABLED"] ??= "false";
-process.env["GGT_CONFIG_DIR"] ??= join(workspaceRoot, "tmp/config");
-process.env["GGT_CACHE_DIR"] ??= join(workspaceRoot, "tmp/cache");
-process.env["GGT_DATA_DIR"] ??= join(workspaceRoot, "tmp/data");
+process.env["GGT_CONFIG_DIR"] ??= workspacePath("tmp/config");
+process.env["GGT_CACHE_DIR"] ??= workspacePath("tmp/cache");
+process.env["GGT_DATA_DIR"] ??= workspacePath("tmp/data");
 
 await import("../src/main.js");

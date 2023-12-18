b01f86c7a08eaa75acd8432f0ec9710b5f9f80e4
Scott Côté <scott.cote@hey.com>
2023-12-17 21:01:04 -0500

    Add installJsonExtensions

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { command as root } from "./commands/root.js";
 import { installErrorHandlers } from "./services/output/report.js";
+import { installJsonExtensions } from "./services/util/json.js";
 
 installErrorHandlers();
+installJsonExtensions();
 
 await root();

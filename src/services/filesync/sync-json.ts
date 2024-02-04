72725527504fa1f3776fecc9e2b657b8d1c9f62c
Scott Côté <scott.cote@hey.com>
2024-02-04 16:27:27 -0500

    WIP - Sun, Feb 04 04:27 PM

---
+++ src/services/filesync/sync-json.ts
@@ -0,0 +1,11 @@
+/**
+ * The state of the filesystem.
+ *
+ * This is persisted to `.gadget/sync.json` within the {@linkcode directory}.
+ */
+export class SyncJson {
+  //   app: App;
+  //   filesVersion: bigint;
+  //   currentEnvironment: string;
+  //   environments: Record<string, { filesVersion: string }>;
+}

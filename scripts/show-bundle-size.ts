7b5b6db30fc22ef330d375a1cc1796a3ca299e8d
Scott Côté <scott.cote@hey.com>
2024-05-21 11:23:45 -0400

    Downgrade @swc-node/register

--- scripts/show-bundle-size.ts
+++ scripts/show-bundle-size.ts
@@ -9,7 +9,7 @@ try {
   process.chdir(workspaceRoot);
   await $`npm run build`;
   await $`npm install --omit=dev`;
-  await $({ stdio: "inherit", shell: true })`du -sh node_modules/* assets/* bin/ lib/ | grep -v '^0' | sort -h`;
+  await $({ stdio: "inherit", shell: true })`du -sh node_modules/* assets/* lib/ | grep -v '^0' | sort -hr`;
 } finally {
   if (!parseBoolean(process.env["CI"])) {
     // re-install dev dependencies
